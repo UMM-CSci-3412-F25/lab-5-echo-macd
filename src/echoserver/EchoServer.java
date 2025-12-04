a5b0dde4937e53a262e549529e59503c46ad9d53
Mackenzie Amoako <amoak017@umn.edu>
2025-12-04 15:22:49 -0600

    EchoServer DONE
    created echo server

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,49 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoServer {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+    String server;
+    // Use "127.0.0.1", i.e., localhost, if no server is specified.
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+    try {
+      // Connect to the server
+      Socket socket = new Socket(server, portNumber);
+      InputStream input = socket.getInputStream();
+      OutputStream output = socket.getOutputStream();
+      
+      int character;
+      while ((character = System.in.read()) != -1) {
+        output.write(character);
+      }
+
+      output.flush();
+      socket.shutdownOutput();
+
+      while ((character = input.read()) != -1) {
+        System.out.write((char)character);
+      }
+
+      System.out.flush();
+      // Close the socket when we're done reading from it
+      socket.close();
+
+    // Provide some minimal error handling.
+    } catch (ConnectException ce) {
+      System.out.println("We were unable to connect to " + server);
+      System.out.println("You should make sure the server is running.");
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
