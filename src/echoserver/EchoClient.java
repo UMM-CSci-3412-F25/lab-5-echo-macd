2eb7984288783c03cece8f069d8df6e91b82b297
Mackenzie Amoako <amoak017@umn.edu>
2025-12-05 16:58:12 -0600

    trying to fix

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -45,33 +45,4 @@ public class EchoClient {
       System.err.println(ioe);
     }
   }
-    try {
-      // Connect to the server
-      Socket socket = new Socket(server, portNumber);
-      InputStream input = socket.getInputStream();
-      OutputStream output = socket.getOutputStream();
-      
-      int character;
-      while ((character = System.in.read()) != -1) {
-        output.write(character);
-      }
-
-      output.flush();
-      socket.shutdownOutput();
-
-      while ((character = input.read()) != -1) {
-        System.out.write((char)character);
-      }
-
-      System.out.flush();
-      // Close the socket when we're done reading from it
-      socket.close();
-
-    // Provide some minimal error handling.
-    } catch (ConnectException ce) {
-      System.out.println("We were unable to connect to " + server);
-      System.out.println("You should make sure the server is running.");
-    } catch (IOException ioe) {
-      System.out.println("We caught an unexpected exception");
-      System.err.println(ioe);
-    }
+   
